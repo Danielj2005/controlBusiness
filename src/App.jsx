6b8a9a3f383fcac9003c1f700aa156432911df48
Danielj2005 <dbarrueta42@gmail.com>
2025-09-25 19:38:22 -0400

    correccion de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Register from './components/Register'
-import Login from './components/login';
+import Login from './components/Login';
 import Home from './components/Home';
 
 
